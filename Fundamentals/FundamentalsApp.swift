cb64081c945e0a0685103d8800cd86b84f35b737
nbmcguire <57973784+nbmcguire@users.noreply.github.com>
2025-02-03 13:00:47 -0500

    Initial Commit

---
+++ Fundamentals/FundamentalsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FundamentalsApp.swift
+//  Fundamentals
+//
+//  Created by Noah McGuire on 2/3/25.
+//
+
+import SwiftUI
+
+@main
+struct FundamentalsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
